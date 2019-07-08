bd5f2ce63079167cd7d79889949924d19fd7c683
GUKSON LIU (ITA-ISDC-ISD-OOCLL/ZHA) <LIUGU2@OOCL.COM>
2019-07-08 11:59:49 +0800

    finish

--- practice/section-1/practice-2.js
+++ practice/section-1/practice-2.js
@@ -1,5 +1,19 @@
 'use strict';
 
 function collectSameElements(collectionA, collectionB) {
-  return '实现练习要求，并改写该行代码。';
+  var result = [];
+    for(var i = 0; i < collectionA.length; i++){
+      for(var j = 0;var k = 0; ; k++){
+        if(collectionB[j][k] === collectionA[i]){
+          result.push(collectionA[i]);
+        }
+        else if{
+           j == collectionB.length;
+           j++;
+        }else{
+          break;
+        }
+      }
+    }
+    return result;
 }
