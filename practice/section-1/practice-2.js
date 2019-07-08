0e86b2daf5ff1b1bfb4c4854c459a40638db6096
GUKSON LIU (ITA-ISDC-ISD-OOCLL/ZHA) <LIUGU2@OOCL.COM>
2019-07-08 12:56:38 +0800

    remember!

--- practice/section-1/practice-2.js
+++ practice/section-1/practice-2.js
@@ -3,15 +3,9 @@
 function collectSameElements(collectionA, collectionB) {
   var result = [];
     for(var i = 0; i < collectionA.length; i++){
-      for(var j = 0, k = 0; ; k++){
-        if(collectionB[j][k] === collectionA[i]){
+      for(var j = 0; j < collectionB[0].length; j++){
+        if(collectionB[0][j] === collectionA[i]){
           result.push(collectionA[i]);
-          continue;
-        }
-        else if(j < collectionB.length){
-             j++;
-        }else{
-          break;
         }
       }
     }
